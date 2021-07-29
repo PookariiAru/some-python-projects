5b2531360d7111d06cc90773efe0029b24d65a9f
Marcin Kucharski <dixone23@gmail.com>
2021-07-29 17:05:58 +0200

    Small code refactoring. Deleted creation of dictionary in favour of generating it inside class function and got rid of combination.py.

--- brute force/bruteforce.py
+++ brute force/bruteforce.py
@@ -1,18 +1,34 @@
-import zipfile 
- 
-filename = 'secret.zip' 
-dictionary = 'dict.txt' 
- 
-password = None 
-file_to_open = zipfile.ZipFile(filename) 
-with open(dictionary, 'r') as f: 
-   for line in f.readlines(): 
-         password = line.strip('\n')
-         #password = b'%s' % password
-         try:
-               file_to_open.extractall(pwd=password.encode()) 
-               password = 'Password found: %s' % password 
-               print(password)
-               break
-         except: 
-               pass 
+import zipfile
+import itertools
+
+
+class Bruteforce:
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def create_combinations(self):
+        alphabets = ["a", "b", "c", "d", "e", "1", "2", "3", "4", "5"]
+        for passlen in [5]:
+            combinations = itertools.product(alphabets, repeat=passlen)
+
+        return ["".join(combination) for combination in combinations]
+
+    def extract_zip(self, pwd: str, zip_file: str):
+        pwd = str.encode(pwd)
+        try:
+            zip_file.extractall(pwd=pwd)
+            return True
+        except Exception:
+            return False
+
+    def bruteforce_zip(self):
+        zip_file = zipfile.ZipFile(self.filename)
+        dictionary = self.create_combinations()
+
+        for line in dictionary:
+            self.extract_zip(line, zip_file)
+
+
+if __name__ == "__main__":
+    test = Bruteforce("secret.zip")
+    test.bruteforce_zip()

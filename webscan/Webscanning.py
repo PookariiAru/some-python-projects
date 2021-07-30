c64f01ab979086f309c05572a5649c182c70d883
PookariiAru <43339041+PookariiAru@users.noreply.github.com>
2021-07-30 10:57:51 +0300

    Update Webscanning.py

--- webscan/Webscanning.py
+++ webscan/Webscanning.py
@@ -6,7 +6,6 @@ url = None
 with open(dictionary, 'r') as f: 
    for line in f.readlines(): 
          url = line.strip('\n')
-         #password = b'%s' % password
          try:
                response = requests.get('http://' + url)
                print(url + " exsists")

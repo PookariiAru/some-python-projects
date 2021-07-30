4398dcedea0803a451f78335057566c9081f6eff
PookariiAru <43339041+PookariiAru@users.noreply.github.com>
2021-07-30 22:58:54 +0300

    Update uselesspaintingui.py

--- uselesspaintingui.py
+++ uselesspaintingui.py
@@ -2,11 +2,14 @@ from tkinter import *
 from tkinter.colorchooser import *
 
 class Paint(object):
-
+    
+    #sets default pen options
     DEFAULT_PEN_SIZE = 5.0
     DEFAULT_COLOR = 'black'
-
+    
     def __init__(self):
+        
+        #some basic gui setting
         self.root = Tk()
 
         self.width = 200
@@ -53,9 +56,12 @@ class Paint(object):
         self.root.mainloop()
 
     def clearbutton(self):
+        #clears the canvas
         self.canvas.delete(ALL)
 
     def setup(self):
+        
+        #setup for the canvas
         self.old_x = None
         self.old_y = None
         self.line_width = self.choose_size_button.get()
@@ -64,6 +70,8 @@ class Paint(object):
         self.active_button = self.pen_button
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
+        
+        #this is used for scrolling the canvas but ig it's not necessary
 '''        
     def scroll_start(self, event):
         self.canvas.scan_mark(event.x, event.y)
@@ -71,23 +79,31 @@ class Paint(object):
     def scroll_move(self, event):
         self.canvas.scan_dragto(event.x, event.y, gain=1)
 '''        
+
     def use_pen(self):
+        #for pen button
         self.activate_button(self.pen_button)
 
     def use_brush(self):
+        #for brush button
         self.activate_button(self.brush_button)
 
     def choose_color(self):
+        #for pen/brush color
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
     def background_color(self):
+        #for bg color
         color = askcolor()[1]
         self.canvas['bg'] = color
 
     def paint(self, event):
+        
         self.line_width = self.choose_size_button.get()
         paint_color = 'white' if self.eraser_on else self.color
+        
+        #for drawing
         if self.old_x and self.old_y:
             self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
                                width=self.line_width, fill=paint_color,

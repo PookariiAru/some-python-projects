7519d96656843afdec23b01b016e008b4e37719a
PookariiAru <43339041+PookariiAru@users.noreply.github.com>
2021-08-01 14:48:57 +0300

    added some comments and modified some

--- uselesspaintingui.py
+++ uselesspaintingui.py
@@ -1,25 +1,25 @@
 from tkinter import *
 from tkinter.colorchooser import *
+from PIL import ImageTk, Image, ImageDraw
+from PIL import *
+import PIL
 
 class Paint(object):
-    
-    #sets default pen options
+
     DEFAULT_PEN_SIZE = 5.0
     DEFAULT_COLOR = 'black'
-    
+
     def __init__(self):
-        
-        #some basic gui setting
         self.root = Tk()
 
         self.width = 200
         self.height = 200
-        #self.white = (255, 255, 255)
+        self.white = (255, 255, 255)
 
         self.root.title("Some Drawing U Kno")
         
-        self.pen_button = Button(self.root, text='PEN', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        self.eraser_button = Button(self.root, text='ERASER', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=0)
 
         self.brush_button = Button(self.root, text='BRUSH', command=self.use_brush)
         self.brush_button.grid(row=0, column=1)
@@ -35,9 +35,8 @@ class Paint(object):
 
         self.clear_button = Button(self.root,text = "CLEAR", command=self.clearbutton)
         self.clear_button.grid(row=0, column=7)
-      
 
-        self.canvas = Canvas(self.root, width=600, height=600)
+        self.canvas = Canvas(self.root, bg='white', width=600, height=600)
         self.canvas.grid(row=2, columnspan=8, sticky="nsew")
 
         #for moving canvas
@@ -60,60 +59,66 @@ class Paint(object):
         self.canvas.delete(ALL)
 
     def setup(self):
-        
-        #setup for the canvas
+        #setup for the default brush/eraser options
         self.old_x = None
         self.old_y = None
         self.line_width = self.choose_size_button.get()
         self.color = self.DEFAULT_COLOR
         self.eraser_on = False
-        self.active_button = self.pen_button
+        self.active_button = self.brush_button
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
-        
-        #this is used for scrolling the canvas but ig it's not necessary
-'''        
+
+        '''
     def scroll_start(self, event):
         self.canvas.scan_mark(event.x, event.y)
 
     def scroll_move(self, event):
         self.canvas.scan_dragto(event.x, event.y, gain=1)
-'''        
-
-    def use_pen(self):
-        #for pen button
-        self.activate_button(self.pen_button)
+        '''
+    def use_eraser(self):
+        #if you press this button, the 'activate_button' will activate the 'eraser_button' and set the 'eraser_on' to True
+        self.activate_button(self.eraser_button, eraser_mode=True)
 
     def use_brush(self):
-        #for brush button
+        #if you press this button, the 'activate_button' will activate the 'brush_button'
         self.activate_button(self.brush_button)
 
     def choose_color(self):
-        #for pen/brush color
+        #color for the brush
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
     def background_color(self):
-        #for bg color
+        
+        #bg color
         color = askcolor()[1]
         self.canvas['bg'] = color
-        
+
     def activate_button(self, some_button, eraser_mode=False):
+        
+        #button configurations if you press one of the buttons
         self.active_button.config(relief=RAISED)
         some_button.config(relief=SUNKEN)
+        
+        #this makes one of the buttons active if you press one of the buttons
         self.active_button = some_button
+        
+        #if this eraser_on is set to True, the eraser will be white
         self.eraser_on = eraser_mode
-        
-    def paint(self, event):
-        
+
+    def paint(self, event): # event, when you are painting with brush
         self.line_width = self.choose_size_button.get()
+        
+        #if this eraser_on is set to True, the eraser will be white
         paint_color = 'white' if self.eraser_on else self.color
         
-        #for drawing
+        # painting with brush event
         if self.old_x and self.old_y:
-            self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
+            self.canvasID = self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
                                width=self.line_width, fill=paint_color,
-                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36, tags="drawingg")
+
         self.old_x = event.x
         self.old_y = event.y
 
